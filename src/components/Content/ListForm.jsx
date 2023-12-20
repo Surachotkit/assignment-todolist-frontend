1555bd6ebea2e714fd8bd9aa5b9457a81e0902ae
Surachotkit <surachot.kit@gmail.com>
2023-12-21 02:17:51 +0700

    connect api get create and delete

--- src/components/Content/ListForm.jsx
+++ src/components/Content/ListForm.jsx
@@ -1,10 +1,54 @@
 import { LuListTodo } from "react-icons/lu";
-import SearchBar from "../../features/SearchBar";
 import ProductList from "../ProductList";
+import { useEffect, useState } from "react";
+import axios from "../../config/axios";
 
 export default function ListForm() {
+  const [dataList, setDataList] = useState([])
+  const [postList, setPostList] = useState({
+    title:""
+  })
+
+
+  const fetchData = async () => {
+    try {
+      const res = await axios.get("/get")
+      setDataList(res.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const postData = async () => {
+    try {
+      await axios.post("/create",postList)
+      fetchData();
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const editData = async () => {
+    
+  }
+
+  const deleteData = async (idx) => {
+    try {
+      await axios.delete(`/delete/${idx}`)
+      setDataList(dataList.filter(el => el.id !== idx))
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+
+  useEffect(() => {
+    fetchData()
+  },[])
+
   return (
-    <div className="flex justify-center  h-screen border">
+    <form className="flex justify-center  h-screen border" >
       <div className="">
         {/* topic */}
         <div className="flex justify-center items-center gap-4 text-5xl font-bold p-5">
@@ -12,28 +56,27 @@ export default function ListForm() {
           <LuListTodo />
         </div>
 
+      
         <div className="relative py-2">
           <input
             type="text"
             className=" w-full h-[40px] rounded-3xl px-5 outline-none"
+            value={postList.title}
+            onChange={(e) => setPostList({ ...postList, title: e.target.value })}
             placeholder="Add new task"
           />
-          <button className="bg-red-500 w-28 h-[40px] rounded-3xl absolute right-0 hover:bg-red-600 text-white">Add</button>
+          <button className="bg-red-500 w-28 h-[40px] rounded-3xl absolute right-0 hover:bg-red-600 text-white" onClick={postData}>Add</button>
         </div>
 
         {/* item */}
-        <div className="flex flex-col bg-white rounded-xl px-5 py-2">
+        <div className="flex flex-col bg-white rounded-xl px-5 py-2 w-[100vh] ">
           <span>Date : 20/12/2023</span>
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-          <ProductList />
-     
+
+          {dataList.map((el,index) => <ProductList key={el.id} index={index} el={el} deleteData={deleteData} />)}
          
         </div>
+
       </div>
-    </div>
+    </form>
   );
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,15 +1,17 @@
 import { MdDelete } from "react-icons/md";
 import { FaRegEdit } from "react-icons/fa";
 
-export default function ProductList() {
+
+export default function ProductList({el,deleteData}) {
+
   return (
     <div className="flex py-2">
       <li className="flex gap-5 items-center justify-evenly w-full text-3xl hover:bg-blue-100 px-6 p-3 rounded-3xl bg-gray-100">
         <div className="w-5 h-5 border-2 border-black"></div>
-        <div>Lorem ipsum dolor </div>
-        <div className="flex justify-center items-center gap-2">
+        <div className="flex flex-1">{el.title} </div>
+        <div className="flex justify-center items-center gap-6">
           <FaRegEdit className="text-xl cursor-pointer" />
-          <MdDelete className="text-2xl cursor-pointer" />
+          <MdDelete className="text-2xl cursor-pointer" onClick={() => deleteData(el.id)}/>
         </div>
       </li>
     </div>
